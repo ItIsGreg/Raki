ad3e6bacd19013e8047fb2db029c25f05bbd1a72
ItIsGreg <g.nageler@web.de>
2025-05-10 14:17:26 +0200

    finished Text Upload Tab

--- projects/frontend/src/components/tutorial/TutorialDrawer.tsx
+++ projects/frontend/src/components/tutorial/TutorialDrawer.tsx
@@ -217,7 +217,96 @@ const TutorialDrawer = ({ isOpen, onOpenChange }: TutorialDrawerProps) => {
                     Do you have the table? You might want to open it and just
                     take a look. After that come back to the tutorial.
                   </p>
-                  <p>Alright, have you seen the table contents? Great.</p>
+                  <p>
+                    Alright, have you seen the table contents? Great. It is just
+                    a bunch of echocardiography reports. Let's upload the text
+                    to the app.
+                  </p>
+
+                  <div className="space-y-4">
+                    <div>
+                      <p className="font-medium">Creating a New Text Set</p>
+                      <p>
+                        In the Text Upload Tab click on the{" "}
+                        <span className="font-semibold">New Text Set</span>{" "}
+                        Button. This lets you name how the texts that we upload
+                        will be stored.
+                      </p>
+                      <p>
+                        You might just want to give a name like{" "}
+                        <span className="font-semibold">Echoreports</span>, or{" "}
+                        <span className="font-semibold">Test Texts</span>. You
+                        can also provide a short description if you like. Click
+                        the <span className="font-semibold">Save</span> button
+                        to create the Text Set.
+                      </p>
+                    </div>
+
+                    <div>
+                      <p className="font-medium">Uploading Your Texts</p>
+                      <p>
+                        Now you can start uploading. Click the{" "}
+                        <span className="font-semibold">upload</span> Button. It
+                        lets you chose between different options on how to
+                        upload the texts. We have our texts in a table, so you
+                        can chose the{" "}
+                        <span className="font-semibold">upload table</span>{" "}
+                        option.
+                      </p>
+                      <p>
+                        Now a Window opens that lets you select a table to
+                        upload. Go to the place where the example echo table you
+                        just downloaded is stored, probably the{" "}
+                        <span className="font-semibold">Downloads</span> folder.
+                      </p>
+                    </div>
+
+                    <div>
+                      <p className="font-medium">Selecting Columns</p>
+                      <p>
+                        After you selected the table for upload a new window
+                        opens that lets you select which columns of the table to
+                        import.
+                      </p>
+                      <p>
+                        First you select the{" "}
+                        <span className="font-semibold">index column</span>.
+                        After this column the texts that you upload will be
+                        named in the app. In this table the index column is
+                        called <span className="font-semibold">Index</span>.
+                      </p>
+                      <p>
+                        Then you select the{" "}
+                        <span className="font-semibold">text column</span>. This
+                        is the column that the texts are actually stored in. In
+                        this table it is just named{" "}
+                        <span className="font-semibold">Text</span>.
+                      </p>
+                      <p>
+                        After you selected both columns, you can use import the
+                        Texts by clicking the{" "}
+                        <span className="font-semibold">Import Texts</span>{" "}
+                        button.
+                      </p>
+                    </div>
+
+                    <div>
+                      <p className="font-medium">Viewing Your Texts</p>
+                      <p>
+                        Very good! Now the text got imported. You are seeing a
+                        list of the imported Texts now. When you click on the
+                        the text is displayed.
+                      </p>
+                      <p>
+                        You can take some time and take a look at the texts.
+                        After that we will continue with the{" "}
+                        <span className="font-semibold">AI Setup</span>. You can
+                        switch to the{" "}
+                        <span className="font-semibold">AI Setup</span> Tab of
+                        the Tutorial.
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </TabsContent>
               <TabsContent value="ai-setup" className="p-4">
